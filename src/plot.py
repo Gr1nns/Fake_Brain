1025302ea989ddde14fd51b207694d65c98138f2
Grinn <152820684+Gr1nns@users.noreply.github.com>
2025-12-28 10:34:03 -0300

    Update plot.py

--- src/plot.py
+++ src/plot.py
@@ -1,1 +1,11 @@
+import matplotlib.pyplot as plt
+from brain import generate_EEG
 
+freqs , mag = generate_EEG()
+
+plt.plot(freqs, mag)
+plt.title("FFT do EEG (4–40 Hz)")
+plt.xlabel("Frequência (Hz)")
+plt.ylabel("Magnitude")
+plt.grid(True)
+plt.show()
